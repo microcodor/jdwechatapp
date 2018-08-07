2a0134cf64d7f4c32512d9719c6bb087244139a8
microcodor <microcodor@163.com>
2018-08-07 11:48:14 +0800

    htaccess

--- application/config.php
+++ application/config.php
@@ -243,8 +243,11 @@ return [
 
     // 视图输出字符串内容替换
     'view_replace_str'       => [
-        '_CSS_'=>'../../public/static/css',
-        '_JS_'=>'../../public/static/js'
+        '__PUBLIC__' => dirname($_SERVER['SCRIPT_NAME']),
+        '__STATIC__' => dirname($_SERVER['SCRIPT_NAME']) . '/static',
+        '__CSS__'    => dirname($_SERVER['SCRIPT_NAME']) . '/static/css',
+        '__JS__'     => dirname($_SERVER['SCRIPT_NAME']) . '/static/js',
+        '__IMG__'    => dirname($_SERVER['SCRIPT_NAME']) . '/static/images',
     ],
 
 ];
